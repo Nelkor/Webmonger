600930ba125bb0a82076e0d3e6c53b0a2e9cc09f
Nelkor <nelkor9855118751@gmail.com>
2018-03-21 15:59:18 +0300

    Update View.php

--- application/core/View.php
+++ application/core/View.php
@@ -15,6 +15,10 @@ class View
 
     public static function error(string $code)
     {
+        /*
+        
+        */
+        
         switch ($code) {
             case '404':
                 header('HTTP/1.0 404 Not Found');
