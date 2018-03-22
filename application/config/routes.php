62f847375dcd08f1d20b5004dd2b045122cc16c8
Nelkor <nelkor9855118751@gmail.com>
2018-03-22 20:33:20 +0300

    Update routes.php

--- application/config/routes.php
+++ application/config/routes.php
@@ -4,7 +4,7 @@ return [
     '' => [
         'controller' => 'main',
         'action' => 'main',
-        'access' => [], // сделать числом. Один роут - одно действие. И для него одно право доступа.
+        'access' => 0,
         'ajax-only' => false
     ]
 ];
