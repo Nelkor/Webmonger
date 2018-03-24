3ef90dae37ff3efc9a8a9de9ed307a2d59a55417
Nelkor <nelkor9855118751@gmail.com>
2018-03-24 20:21:07 +0300

    Update Controller.php

--- application/core/Controller.php
+++ application/core/Controller.php
@@ -1,11 +1,13 @@
 <?php
 
 require_once 'application/classes/Session.php';
+require_once 'application/classes/Cache.php';
 
 abstract class Controller
 {
     protected $session;
     protected $request;
+    protected $cache;
 
     protected function loadModel(string $name)
     {
@@ -23,13 +25,13 @@ abstract class Controller
     protected function respond(string $page)
     {
         if (filter_has_var(INPUT_GET, 'ajax')) {
-            View::response('ok', [
+            Output::json('ok', [
                 'html' => $page,
                 'url' => $this->request
             ]);
         }
 
-        echo View::render('wrapper', ['page' => $page]);
+        echo Output::html('wrapper', ['page' => $page]);
     }
 
     protected function redirect(string $path)
@@ -46,18 +48,19 @@ abstract class Controller
     public function __construct()
     {
         $this->session = Session::handle();
+        $this->cache = Cache::handle();
     }
 
     public function __invoke(string $action, int $access, string $request)
     {
         $rights = $this->session->get_int('login_rights');
 
-        if ( ! $access || ($rights & $access == $access)) { // TODO проверить / Возможно, снять лишние скобки
+        if ( ! $access || ($rights & $access)) {
             $this->request = $request;
 
             $this->$action();
         } else {
-            View::error('403');
+            Output::error('403');
         }
     }
 }
